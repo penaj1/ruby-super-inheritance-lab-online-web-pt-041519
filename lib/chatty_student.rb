3b5620f580f258681a9a0ee78ae34f9233507a1c
Janice Pena <jipena94@gmail.com>
2019-05-26 02:28:06 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -9,15 +9,6 @@ class ChattyStudent < Student
   
   def raise_hand
     super
-    super
-    super
-    super
-    super
-    super
-    super
-    super
-    super
-    super
     puts "Pick me!"
   end
   
